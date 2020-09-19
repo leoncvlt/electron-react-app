1295bf619490ece53880ad844ecd8e8c25ead1dc
Leonardo Cavaletti <impeto.blu@gmail.com>
2020-09-19 16:53:20 +0100

    Updated Readme and About page

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # electron-react-app
-A boilerplate for a react-based electron application, with local database support and fancy touches like dark / light theme and offline detection
+A boilerplate for a react-based electron desktop application, with local database support and fancy touches like dark / light theme and offline detection.
 
 The application is set up with:
 - [Electron](https://www.electronjs.org/)
 - [Create React App](https://github.com/facebook/create-react-app) for easy react bootstrapping
-- React Router for internal page routing
+- [React Router](https://reactrouter.com/) for internal page routing
 - [Blueprint](https://blueprintjs.com/) as UI framework
 - [React-JSS](https://cssinjs.org/react-jss) as styling solution
 - [minim-json-db](https://www.npmjs.com/package/minim-json-db) for database integration
@@ -12,4 +12,6 @@ The application is set up with:
 ## Development
 `npm run start` will start the a development server with hot reload, as well with an electron window pointing to the same server, so you can see the changes in real-time in the electron window.
 
-`npm run build` will package the electron application using [electron-builder](https://www.electron.build/) as a stand-alone executable. `npm run dist` will do the same, but creating an installer instead.
+`npm run build` will package the electron application using [electron-builder](https://www.electron.build/) as a stand-alone executable. 
+
+`npm run dist` will package the application as well, but creating an installer instead.
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,49 @@
 import React, { useState } from "react";
 import { Button } from "@blueprintjs/core";
 
-export const AboutPage = () => <></>;
+export const AboutPage = () => (
+  <>
+    <h1 id="electron-react-app">electron-react-app</h1>
+    <p>
+      A boilerplate for a react-based electron desktop application, with local database support and
+      fancy touches like dark / light theme and offline detection.
+    </p>
+    <p>The application is set up with:</p>
+    <ul>
+      <li>
+        <a href="https://www.electronjs.org/" target="_blank">Electron</a>
+      </li>
+      <li>
+        <a href="https://github.com/facebook/create-react-app" target="_blank">Create React App</a> for easy react
+        bootstrapping
+      </li>
+      <li>
+        <a href="https://reactrouter.com/" target="_blank">React Router</a> for internal page routing
+      </li>
+      <li>
+        <a href="https://blueprintjs.com/" target="_blank">Blueprint</a> as UI framework
+      </li>
+      <li>
+        <a href="https://cssinjs.org/react-jss" target="_blank">React-JSS</a> as styling solution
+      </li>
+      <li>
+        <a href="https://www.npmjs.com/package/minim-json-db" target="_blank">minim-json-db</a> for database
+        integration
+      </li>
+    </ul>
+    <h2 id="development">Development</h2>
+    <p>
+      <code>npm run start</code> will start the a development server with hot reload, as well with
+      an electron window pointing to the same server, so you can see the changes in real-time in the
+      electron window.
+    </p>
+    <p>
+      <code>npm run build</code> will package the electron application using{" "}
+      <a href="https://www.electron.build/">electron-builder</a> as a stand-alone executable.{" "}
+    </p>
+    <p>
+      <code>npm run dist</code> will package the application as well, but creating an installer
+      instead.
+    </p>
+  </>
+);
